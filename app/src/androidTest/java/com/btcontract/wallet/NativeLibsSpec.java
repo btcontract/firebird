1986d884e16594bfd403face84c471ff48e14291
engenegr <evdokimov.ilya@gmail.com>
2020-08-23 12:19:59 +0200

    cleaned. submodule enabled

--- app/src/androidTest/java/com/btcontract/wallet/NativeLibsSpec.java
+++ app/src/androidTest/java/com/btcontract/wallet/NativeLibsSpec.java
@@ -2,6 +2,7 @@ package com.btcontract.wallet;
 
 import static org.junit.Assert.*;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import com.blockstream.libwally.Wally;
 import org.bitcoin.Secp256k1Context;
 import org.junit.runner.RunWith;
@@ -16,8 +17,14 @@ public class NativeLibsSpec {
         assertTrue(Wally.isEnabled());
     }
 
-    @Test(timeout = 5000)
+    @Test(timeout = 50000)
     public void scryptWorksFast() {
+        assertTrue(Wally.isEnabled());
         WalletApp.scryptDerive("hello@email.com", "password123");
+    }
+
+    @Test(timeout = 5000)
+    public void secpWorksFast() {
+        WalletApp.getRandomMnemonic();
     }
 }
